e2a6f285540ab2f39821e777cceb8d425391b6d5
Abhinav Gujjar <abhinavgujjar@gmail.com>
2015-02-05 11:29:12 +0530

    services introduction

--- app/app.js
+++ app/app.js
@@ -2,6 +2,31 @@
 var app = angular.module('wfis', ['greeting']); //empty braces denote no dependencies on other modules!
 
 
+//value service
+app.value('uiConfig', {
+	textLimit : 200,
+	numberOfItems : 5,
+	expensive : 10000
+});
+
+app.factory('votingService', function(){
+
+	//return the service
+	return {
+		up : function(hotel){
+			hotel.rating ++;
+		},
+		down : function(hotel){
+			hotel.rating = hotel.rating || 0;
+
+			if (hotel.rating > 0 ){
+				hotel.rating --;
+			}
+		}
+	}
+
+})
+
 
 app.filter('numberToArray', function(){
 	//return a function that can be applied for each value
@@ -9,4 +34,22 @@ app.filter('numberToArray', function(){
 	return function(input){
 		return new Array(input);
 	}
+});
+
+app.filter('paginate', function(){
+	return function(input, numberOfItems, pageNumber){
+		//should select the correct page and give back 
+		var output = input.slice((pageNumber -1 ) * numberOfItems, (pageNumber*numberOfItems));
+
+		return output;
+	}
 })
+
+
+app.filter('traditionalize', function(){
+	//return a function that can be applied for each value
+	//input will be the value on which the filter has been applied
+	return function(input){
+		return input.replace('Bangalore', 'Bengaluru');
+	}
+});
