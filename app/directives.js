aa7823c13a9e51c49c61a75e514b64838548dca8
Abhinav Gujjar <abhinavgujjar@gmail.com>
2015-02-06 16:00:11 +0530

    custom validation and communication between two directives

--- app/directives.js
+++ app/directives.js
@@ -29,7 +29,7 @@ app.directive('quickAdd', function() {
 	return {
 		restrict: 'E',
 		templateUrl: 'partials/quick.html',
-		controller : 'addController'
+		controller: 'addController'
 	}
 })
 
@@ -37,35 +37,54 @@ app.directive('favs', function() {
 	return {
 		restrict: 'E',
 		templateUrl: 'partials/favs.html',
-		controller : 'favsController'
+		controller: 'favsController'
 	}
 })
 
 app.directive('panel', function() {
 	return {
 		restrict: 'E',
-		transclude : true,
+		transclude: true,
 		templateUrl: 'partials/panel.html',
-		scope :{
-			heading  :'@',
-			modifier :  '@'
+		scope: {
+			heading: '@',
+			modifier: '@'
 		}
 	}
 });
 
-app.directive('zoom', function(){
-	return{
-		restrict : 'A',
-		link : function(scope, element, attr, ctrl){
+app.directive('zoom', function() {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attr, ctrl) {
 
-			
-			element.on('mouseenter', function(){
+
+			element.on('mouseenter', function() {
 
 				element.css({
-					'font-size' : '42px'
+					'font-size': '42px'
 				});
 
 			})
+		}
+	}
+});
+
+app.directive('onlyEven', function() {
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		link: function(scope, element, attr, ctrl) {
+			ctrl.$parsers.unshift(checkForEven);
+
+			function checkForEven(viewValue) {
+				if (parseInt(viewValue) % 2 === 0) {
+					ctrl.$setValidity('evenNumber', true);
+				} else {
+					ctrl.$setValidity('evenNumber', false);
+				}
+				return viewValue;
+			}
 		}
 	}
 })
@@ -75,15 +94,15 @@ app.directive('carousel', function() {
 		restrict: 'E',
 		templateUrl: 'partials/carousel.html',
 		scope: {
-			images : '=images',
-			selectedImage : '='
+			images: '=images',
+			selectedImage: '='
 		},
 		controller: function($scope) {
 			$scope.index = 0;
 
 			$scope.next = function() {
 				$scope.index++;
-				if( $scope.index >= $scope.images.length){
+				if ($scope.index >= $scope.images.length) {
 					$scope.index = 0;
 				}
 
@@ -92,7 +111,7 @@ app.directive('carousel', function() {
 
 			$scope.prev = function() {
 				$scope.index--;
-				if ( $scope.index < 0 ){
+				if ($scope.index < 0) {
 					$scope.index = $scope.images.length - 1;
 				}
 
